fb7c3abb40b26607be76573c048c4cc9fbfa7317
meszarosadorjan-sudo <meszaros.adorjan@gmail.com>
2026-05-24 10:08:01 +0200

    Create Auto.py

---
+++ Auto.py
@@ -0,0 +1,16 @@
+from abc import ABC,abstractmethod 
+# Definiálja az autó alapvető attribútumait (rendszám, típus, bérleti díj).
+class Auto(ABC):
+    def __init__(self,rendszam, tipus, berleti_dij):
+        self._rendszam = rendszam
+        self._tipus = tipus
+        self._berleti_dij = berleti_dij
+        self._foglalasok = []
+    
+@abstractmethod
+def book_room(self):
+    pass
+
+@abstractmethod
+def unbook_room(self):
+    pass   
